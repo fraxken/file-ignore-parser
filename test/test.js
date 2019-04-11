3899c61d6b36e1163a62549a05afb0cb7ffae30f
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-04-11 15:40:20 +0200

    test: add full test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,26 @@
+// Require Node.js Dependencies
+const { join } = require("path");
+
 // Require Third-party Dependencies
 const avaTest = require("ava");
+const is = require("@slimio/is");
 
-avaTest("Husky passed Test", (assert) => {
-    assert.pass();
+// Require Internal Dependencies
+const parser = require("../");
+
+avaTest("Export must be an Asynchronous method", (assert) => {
+    assert.true(is.asyncFunction(parser));
+});
+
+avaTest("Parse .testfile and check deep content", async(assert) => {
+    const ret = await parser(join(__dirname, ".testfile"));
+    assert.true(is.set(ret));
+    assert.deepEqual([...ret], ["/hello", "mdr.js", "yo.js"]);
+});
+
+avaTest("location must be a string", async(assert) => {
+    await assert.throwsAsync(parser(10), {
+        instanceOf: TypeError,
+        message: "location must be a string"
+    });
 });
