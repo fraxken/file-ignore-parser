37944fa544fa2e9a76b05c55906a75906e45ca19
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2020-02-19 13:42:42 +0100

    refactor: fix linter issues & enhance code

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
+"use strict";
+
 // Require Node.js Dependencies
 const { readFile } = require("fs").promises;
 
 /**
  * @async
- * @generator
- * @func parseIgnoreFile
- * @desc Parse .npmignore or .gitignore file and return each lines as a Set
- * @param {!String} location file location
- * @returns {Promise<Set<String>>}
+ * @function parseIgnoreFile
+ * @description Parse .npmignore or .gitignore file and return each lines as a Set
+ * @param {!string} location file location
+ * @returns {Promise<Set<string>>}
  *
  * @throws {TypeError} location must be a string
  */
 async function parseIgnoreFile(location) {
-    if (typeof location !== "string") {
+    const locationStr = String(location);
+    if (Number.isNaN(locationStr)) {
         throw new TypeError("location must be a string");
     }
 
-    const str = await readFile(location, { encoding: "utf8" });
+    const str = await readFile(locationStr, { encoding: "utf8" });
     const lines = str.split(/\r?\n/).filter((line) => line.trim() !== "" && line.charAt(0) !== "#");
 
     return new Set(lines);
